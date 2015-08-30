d8fa762d9a9e66f57992dea20ee202d893d5bfbc
bianlanzhou <bianlanzhou@gmail.com>
2015-08-30 22:36:33 +0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bianlzWebFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bianlzWebFrontApp
+ */
+angular.module('bianlzWebFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bianlzWebFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bianlzWebFrontApp
+ */
+angular.module('bianlzWebFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
